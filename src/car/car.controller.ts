7dc6a9d86884eafce91e5fffb5b03889e923594c
Максим <maksim@MacBook-Pro-Maksim.local>
2024-03-11 12:14:39 +0700

    add: database collection

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CarService } from './car.service';
-import { CreateCarDto } from './dto/create-car.dto';
-import { UpdateCarDto } from './dto/update-car.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { CarService } from './car.service'
+import { CreateCarDto } from './dto/create-car.dto'
+import { UpdateCarDto } from './dto/update-car.dto'
 
 @Controller('car')
 export class CarController {
@@ -9,26 +9,26 @@ export class CarController {
 
   @Post()
   create(@Body() createCarDto: CreateCarDto) {
-    return this.carService.create(createCarDto);
+    return this.carService.create(createCarDto)
   }
 
   @Get()
   findAll() {
-    return this.carService.findAll();
+    return this.carService.findAll()
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.carService.findOne(+id);
+    return this.carService.findOne(+id)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCarDto: UpdateCarDto) {
-    return this.carService.update(+id, updateCarDto);
+    return this.carService.update(+id, updateCarDto)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.carService.remove(+id);
+    return this.carService.remove(+id)
   }
 }
--- src/car/entities/car.entity.ts
+++ src/car/entities/car.entity.ts
@@ -1,1 +1,36 @@
-export class Car {}
+import { User } from 'src/users/entities/user.entity'
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  BaseEntity,
+} from 'typeorm'
+
+@Entity('cars')
+export class Car extends BaseEntity {
+  @PrimaryGeneratedColumn({ name: 'cars_id' })
+  id: number
+
+  @Column()
+  brand: string
+
+  @Column()
+  model: string
+
+  @Column()
+  year: string
+
+  @ManyToOne(() => User, (user) => user.cars, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: User
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+}
